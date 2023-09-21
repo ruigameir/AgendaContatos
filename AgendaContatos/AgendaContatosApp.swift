a32d6aafeae1601762eb3c1ed2a29c293cb0ea26
ruigameir <ruigameiro11@gmail.com>
2023-09-21 20:17:55 +0100

    Initial Commit

---
+++ AgendaContatos/AgendaContatosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaContatosApp.swift
+//  AgendaContatos
+//
+//  Created by Rui Gameiro on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AgendaContatosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
